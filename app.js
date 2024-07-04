0e40029fdb4f248b36a937425328555f443e43aa
Agustin A. Brizuela <agustin01brizuela@gmail.com>
2024-07-04 17:00:38 -0300

    carga de imagenes

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const app = express();
+const dotenv = require('dotenv');
 const userRoutes = require('./src/routes/userRoutes');
 const movieRoutes = require('./src/routes/movieRoutes');
+
+dotenv.config();
+
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -2,9 +2,36 @@ const express = require('express');
 const router = express.Router();
 const movieController = require('../controllers/movieController');
 const auth = require('../middleware/auth');
+const multer = require('multer');
+const axios = require('axios');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+router.post('/upload', auth, upload.single('image'), async (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No se pudo subir la imagen' });
+  }
+
+  try {
+    const response = await axios.post('https://api.imgur.com/3/image', req.file.buffer, {
+      headers: {
+        Authorization: `Client-ID ${process.env.IMGUR_CLIENT_ID}`,
+        'Content-Type': 'multipart/form-data'
+      }
+    });
+
+    res.json({ url: response.data.data.link });
+  } catch (error) {
+    res.status(500).json({ error: 'Error subiendo la imagen a Imgur' });
+  }
+});
 
 // Rutas para CRUD de películas
-router.post('/movies',auth, movieController.createMovie);
+router.post('/movies', auth, movieController.createMovie);
 router.get('/movies', auth, movieController.getMovies);
 router.get('/movies/:id', auth, movieController.getMovieById);
 router.put('/movies/:id', auth, movieController.updateMovie);
