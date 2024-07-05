186af0d597a668f2b03e4b53419cd1b01fc5b5a5
Agustin A. Brizuela <agustin01brizuela@gmail.com>
2024-07-04 23:25:14 -0300

    cambios para subir imagenes a imgur

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -5,27 +5,43 @@ const auth = require('../middleware/auth');
 const multer = require('multer');
 const axios = require('axios');
 const dotenv = require('dotenv');
+const FormData = require('form-data');
 
 dotenv.config();
 
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage }).any();
 
-router.post('/upload', auth, upload.single('image'), async (req, res) => {
-  if (!req.file) {
+router.post('/upload', auth, upload, async (req, res) => {
+  console.log('Files:', req.files);
+  
+  if (!req.files || req.files.length === 0) {
     return res.status(400).json({ error: 'No se pudo subir la imagen' });
   }
 
-  try {
-    const response = await axios.post('https://api.imgur.com/3/image', req.file.buffer, {
-      headers: {
-        Authorization: `Client-ID ${process.env.IMGUR_CLIENT_ID}`,
-        'Content-Type': 'multipart/form-data'
-      }
-    });
+  const file = req.files[0];
 
+  try {
+    console.log('Intentando subir a Imgur...');
+    
+    // Crea una instancia de FormData
+    const formData = new FormData();
+    formData.append('image', file.buffer, file.originalname);
+
+    // Configura los encabezados de la solicitud
+    const headers = {
+      Authorization: `Client-ID ${process.env.IMGUR_CLIENT_ID}`,
+      ...formData.getHeaders()
+    };
+
+    // Envía la solicitud POST a Imgur
+    const response = await axios.post('https://api.imgur.com/3/image', formData, { headers });
+
+    console.log('Respuesta de Imgur:', response.data);
     res.json({ url: response.data.data.link });
   } catch (error) {
+    console.error('Error completo:', error);
+    console.error('Error subiendo la imagen a Imgur:', error.response ? error.response.data : error.message);
     res.status(500).json({ error: 'Error subiendo la imagen a Imgur' });
   }
 });
